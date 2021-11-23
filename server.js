b6022f2c88672ceaa6319d2569da3cbadb493b34
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-23 09:42:04 +0330

    parse body

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use("/api", route);
 
 app.get("/", (req, res) => {

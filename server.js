757447e39c579d9b5da0e44274eb3faa535762dd
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-23 09:46:57 +0330

    Add extended option to body parser

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", route);
 

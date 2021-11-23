c449db2c27c3addf828de39b2cff0d24453fced0
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-23 09:30:58 +0330

    Get accounts route

--- router.js
+++ router.js
@@ -2,8 +2,19 @@ const express = require("express");
 const accounts = require("./database");
 const route = express.Router();
 
+/**
+ * @route GET /api/accounts
+ * @desc Get all accounts
+ * @access Public
+ * @returns {object} 200 - An array of accounts
+ * @returns {Error}  500 - Unexpected error
+ */
 route.get("/accounts", (req, res) => {
-  res.json({ userData: accounts });
+  try {
+    res.json({ userData: accounts });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = route;

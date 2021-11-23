f7729fb9fdf19002c2461eb6537b00255cdd8480
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-23 10:31:27 +0330

    Update An account route

--- router.js
+++ router.js
@@ -40,12 +40,42 @@ route.post("/accounts", (req, res) => {
  * @access Public
  * @returns {object} 200 - An account
  * @returns {Error}  500 - Unexpected error
+ * @returns {Error}  404 - Account not found
  */
 route.get("/accounts/:id", (req, res) => {
   try {
     const accountId = +req.params.id;
     const getAccount = accounts.find((account) => account.id === accountId);
     if (getAccount) {
+      res.json({ userData: getAccount });
+    } else {
+      res.status(404).send("Account not found");
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/**
+ * @route PUT /api/accounts/:id
+ * @desc Update an account
+ * @access Public
+ * @returns {object} 200 - An account
+ * @returns {Error}  500 - Unexpected error
+ * @returns {Error}  404 - Account not found
+ */
+route.put("/accounts/:id", (req, res) => {
+  try {
+    const accountId = +req.params.id;
+    const getAccount = accounts.find((account) => account.id === accountId);
+    if (getAccount) {
+      const { username, email, gender, status } = req.body;
+
+      if (username) getAccount.username = username;
+      if (email) getAccount.email = email;
+      if (gender) getAccount.gender = gender;
+      if (status) getAccount.status = status;
+
       res.json({ userData: getAccount });
     } else {
       res.status(404).send("Account not found");
